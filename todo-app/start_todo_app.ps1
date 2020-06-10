1cec955e515d6f205b8fb89d1c7d86ce6ffe0932
Adithya NR <adithyabhat17.ab@gmail.com>
2020-06-10 10:53:54 +0530

    :tada: created scripts for starting and stopping containers

--- todo-app/start_todo_app.ps1
+++ todo-app/start_todo_app.ps1
@@ -1,2 +1,3 @@
-docker build -t todo-app:v1 .
-docker run -p 8080:8080 todo-app:v1
+#docker build -t todo-app:v1 .
+#docker run -p 8080:8080 todo-app:v1
+docker-compose -f docker-compose.yml up
--- todo-app/start_todo_app.sh
+++ todo-app/start_todo_app.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-docker build -t todo-app:v1 .
-docker run -p 8080:8080 todo-app:v1
+#docker build -t todo-app:v1 .
+#docker run -p 8080:8080 todo-app:v1
+docker-compose -f docker-compose.yml up
